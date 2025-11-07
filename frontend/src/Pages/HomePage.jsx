61ad9eaa28811da6347fe2c7f1627ba897e6cee4
adity1raut <araut7798@gmail.com>
2025-11-07 17:37:29 +0530

    ADD The Some logo
    Signed-off-by: adity1raut <araut7798@gmail.com>

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -22,29 +22,55 @@ const HomePage = () => {
         {/* Corner designs - Hidden on mobile/tablet */}
         {/* Top-left corner */}
         <div className="absolute top-0 left-0 w-64 h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 overflow-hidden hidden lg:block opacity-80 lg:opacity-100">
-          <svg className="absolute top-0 left-0 w-full h-full" viewBox="0 0 384 384">
+          <svg className="absolute top-0 left-0 w-full h-full" viewBox="0 0 256 256">
+            {/* Glow effect */}
             <defs>
-              <filter id="glow">
-                <feGaussianBlur stdDeviation="4" result="coloredBlur" />
+              <filter id="glowTopLeft">
+                <feGaussianBlur stdDeviation="4" result="coloredBlur"/>
                 <feMerge>
-                  <feMergeNode in="coloredBlur" />
-                  <feMergeNode in="SourceGraphic" />
+                  <feMergeNode in="coloredBlur"/>
+                  <feMergeNode in="SourceGraphic"/>
                 </feMerge>
               </filter>
-              <linearGradient id="redGrad" x1="0%" y1="0%" x2="100%" y2="100%">
-                <stop offset="0%" style={{ stopColor: "#ff5757", stopOpacity: 1 }} />
-                <stop offset="100%" style={{ stopColor: "#ff3333", stopOpacity: 1 }} />
+              <linearGradient id="redGradTopLeft" x1="0%" y1="0%" x2="100%" y2="100%">
+                <stop offset="0%" style={{stopColor: '#ff5757', stopOpacity: 1}} />
+                <stop offset="100%" style={{stopColor: '#ff3333', stopOpacity: 1}} />
               </linearGradient>
             </defs>
-            <path d="M 0 0 L 360 0 L 300 45 L 270 45 L 255 60 L 75 60 L 60 75 L 45 75 L 0 120 Z" fill="url(#redGrad)" filter="url(#glow)" />
-            <path d="M 15 15 L 345 15 L 288 54 L 261 54 L 249 66 L 81 66 L 69 78 L 54 78 L 15 117 Z" fill="#1e293b" />
-            <path d="M 30 30 L 330 30 L 276 63 L 252 63 L 243 72 L 87 72 L 78 81 L 63 81 L 30 114 Z" fill="none" stroke="#ff5757" strokeWidth="1" opacity="0.5" />
+            
+            {/* Main red frame */}
+            <path d="M 0 0 L 240 0 L 200 30 L 180 30 L 170 40 L 50 40 L 40 50 L 30 50 L 0 80 Z" 
+                  fill="url(#redGradTopLeft)" 
+                  filter="url(#glowTopLeft)" />
+            
+            {/* Inner dark frame */}
+            <path d="M 10 10 L 230 10 L 192 36 L 174 36 L 166 44 L 54 44 L 46 52 L 36 52 L 10 78 Z" 
+                  fill="#1e293b" />
+            
+            {/* Accent lines */}
+            <path d="M 20 20 L 220 20 L 184 42 L 168 42 L 162 48 L 58 48 L 52 54 L 42 54 L 20 76 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1" 
+                  opacity="0.5" />
+            
+            {/* Corner cutout detail */}
+            <path d="M 35 25 L 55 25 L 55 35 L 35 35 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1.5" 
+                  opacity="0.6" />
+            <circle cx="45" cy="30" r="2" fill="#ff5757" opacity="0.8" />
+            
+            {/* Tech details */}
+            <line x1="70" y1="28" x2="140" y2="28" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
+            <line x1="70" y1="32" x2="120" y2="32" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
           </svg>
         </div>
 
         {/* Top-right corner */}
         <div className="absolute top-0 right-0 w-64 h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 overflow-hidden hidden lg:block opacity-80 lg:opacity-100">
-          <svg className="absolute top-0 right-0 w-full h-full" viewBox="0 0 384 384">
+          <svg className="absolute top-0 right-0 w-full h-full" viewBox="0 0 256 256">
             <defs>
               <filter id="glowTopRight">
                 <feGaussianBlur stdDeviation="4" result="coloredBlur" />
@@ -58,15 +84,40 @@ const HomePage = () => {
                 <stop offset="100%" style={{ stopColor: "#ff3333", stopOpacity: 1 }} />
               </linearGradient>
             </defs>
-            <path d="M 384 0 L 24 0 L 84 45 L 114 45 L 129 60 L 309 60 L 324 75 L 339 75 L 384 120 Z" fill="url(#redGradTopRight)" filter="url(#glowTopRight)" />
-            <path d="M 369 15 L 39 15 L 96 54 L 123 54 L 135 66 L 303 66 L 315 78 L 330 78 L 369 117 Z" fill="#1e293b" />
-            <path d="M 354 30 L 54 30 L 108 63 L 132 63 L 141 72 L 297 72 L 306 81 L 321 81 L 354 114 Z" fill="none" stroke="#ff5757" strokeWidth="1" opacity="0.5" />
+            
+            {/* Main red frame */}
+            <path d="M 256 0 L 16 0 L 56 30 L 76 30 L 86 40 L 206 40 L 216 50 L 226 50 L 256 80 Z" 
+                  fill="url(#redGradTopRight)" 
+                  filter="url(#glowTopRight)" />
+            
+            {/* Inner dark frame */}
+            <path d="M 246 10 L 26 10 L 64 36 L 82 36 L 90 44 L 202 44 L 210 52 L 220 52 L 246 78 Z" 
+                  fill="#1e293b" />
+            
+            {/* Accent lines */}
+            <path d="M 236 20 L 36 20 L 72 42 L 88 42 L 94 48 L 198 48 L 204 54 L 214 54 L 236 76 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1" 
+                  opacity="0.5" />
+            
+            {/* Corner cutout detail */}
+            <path d="M 221 25 L 201 25 L 201 35 L 221 35 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1.5" 
+                  opacity="0.6" />
+            <circle cx="211" cy="30" r="2" fill="#ff5757" opacity="0.8" />
+            
+            {/* Tech details */}
+            <line x1="116" y1="28" x2="186" y2="28" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
+            <line x1="136" y1="32" x2="186" y2="32" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
           </svg>
         </div>
 
         {/* Bottom-right corner */}
         <div className="absolute bottom-0 right-0 w-64 h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 overflow-hidden hidden lg:block opacity-80 lg:opacity-100">
-          <svg className="absolute bottom-0 right-0 w-full h-full" viewBox="0 0 384 384">
+          <svg className="absolute bottom-0 right-0 w-full h-full" viewBox="0 0 256 256">
             <defs>
               <filter id="glowBottomRight">
                 <feGaussianBlur stdDeviation="4" result="coloredBlur" />
@@ -80,15 +131,40 @@ const HomePage = () => {
                 <stop offset="100%" style={{ stopColor: "#ff3333", stopOpacity: 1 }} />
               </linearGradient>
             </defs>
-            <path d="M 384 384 L 24 384 L 84 339 L 114 339 L 129 324 L 309 324 L 324 309 L 339 309 L 384 264 Z" fill="url(#redGradBottomRight)" filter="url(#glowBottomRight)" />
-            <path d="M 369 369 L 39 369 L 96 330 L 123 330 L 135 318 L 303 318 L 315 306 L 330 306 L 369 267 Z" fill="#1e293b" />
-            <path d="M 354 354 L 54 354 L 108 321 L 132 321 L 141 312 L 297 312 L 306 303 L 321 303 L 354 270 Z" fill="none" stroke="#ff5757" strokeWidth="1" opacity="0.5" />
+            
+            {/* Main red frame */}
+            <path d="M 256 256 L 16 256 L 56 226 L 76 226 L 86 216 L 206 216 L 216 206 L 226 206 L 256 176 Z" 
+                  fill="url(#redGradBottomRight)" 
+                  filter="url(#glowBottomRight)" />
+            
+            {/* Inner dark frame */}
+            <path d="M 246 246 L 26 246 L 64 220 L 82 220 L 90 212 L 202 212 L 210 204 L 220 204 L 246 178 Z" 
+                  fill="#1e293b" />
+            
+            {/* Accent lines */}
+            <path d="M 236 236 L 36 236 L 72 214 L 88 214 L 94 208 L 198 208 L 204 202 L 214 202 L 236 180 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1" 
+                  opacity="0.5" />
+            
+            {/* Corner cutout detail */}
+            <path d="M 221 231 L 201 231 L 201 221 L 221 221 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1.5" 
+                  opacity="0.6" />
+            <circle cx="211" cy="226" r="2" fill="#ff5757" opacity="0.8" />
+            
+            {/* Tech details */}
+            <line x1="116" y1="228" x2="186" y2="228" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
+            <line x1="136" y1="224" x2="186" y2="224" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
           </svg>
         </div>
 
         {/* Bottom-left corner */}
         <div className="absolute bottom-0 left-0 w-64 h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 overflow-hidden hidden lg:block opacity-80 lg:opacity-100">
-          <svg className="absolute bottom-0 left-0 w-full h-full" viewBox="0 0 384 384">
+          <svg className="absolute bottom-0 left-0 w-full h-full" viewBox="0 0 256 256">
             <defs>
               <filter id="glowBottomLeft">
                 <feGaussianBlur stdDeviation="4" result="coloredBlur" />
@@ -102,9 +178,34 @@ const HomePage = () => {
                 <stop offset="100%" style={{ stopColor: "#ff3333", stopOpacity: 1 }} />
               </linearGradient>
             </defs>
-            <path d="M 0 384 L 360 384 L 300 339 L 270 339 L 255 324 L 75 324 L 60 309 L 45 309 L 0 264 Z" fill="url(#redGradBottomLeft)" filter="url(#glowBottomLeft)" />
-            <path d="M 15 369 L 345 369 L 288 330 L 261 330 L 249 318 L 81 318 L 69 306 L 54 306 L 15 267 Z" fill="#1e293b" />
-            <path d="M 30 354 L 330 354 L 276 321 L 252 321 L 243 312 L 87 312 L 78 303 L 63 303 L 30 270 Z" fill="none" stroke="#ff5757" strokeWidth="1" opacity="0.5" />
+            
+            {/* Main red frame */}
+            <path d="M 0 256 L 240 256 L 200 226 L 180 226 L 170 216 L 50 216 L 40 206 L 30 206 L 0 176 Z" 
+                  fill="url(#redGradBottomLeft)" 
+                  filter="url(#glowBottomLeft)" />
+            
+            {/* Inner dark frame */}
+            <path d="M 10 246 L 230 246 L 192 220 L 174 220 L 166 212 L 54 212 L 46 204 L 36 204 L 10 178 Z" 
+                  fill="#1e293b" />
+            
+            {/* Accent lines */}
+            <path d="M 20 236 L 220 236 L 184 214 L 168 214 L 162 208 L 58 208 L 52 202 L 42 202 L 20 180 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1" 
+                  opacity="0.5" />
+            
+            {/* Corner cutout detail */}
+            <path d="M 35 231 L 55 231 L 55 221 L 35 221 Z" 
+                  fill="none" 
+                  stroke="#ff5757" 
+                  strokeWidth="1.5" 
+                  opacity="0.6" />
+            <circle cx="45" cy="226" r="2" fill="#ff5757" opacity="0.8" />
+            
+            {/* Tech details */}
+            <line x1="70" y1="228" x2="140" y2="228" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
+            <line x1="70" y1="224" x2="120" y2="224" stroke="#ff5757" strokeWidth="1" opacity="0.4" />
           </svg>
         </div>
 
